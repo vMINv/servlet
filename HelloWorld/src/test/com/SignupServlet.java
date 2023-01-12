333705c3f8d6d0fc106b03785cc2201c4eb1cbdf
ShimHyunmin <nimnuyhmihs@gmail.com>
2023-01-12 17:21:51 +0900

    20230112

---
+++ HelloWorld/src/test/com/SignupServlet.java
@@ -0,0 +1,44 @@
+package test.com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class signupServlet
+ */
+@WebServlet("/signupServlet")
+public class SignupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String res = "<html><h3>User ID: " + request.getParameter("id") + "</h3>";
+		res += "<h3>Password: " + request.getParameter("ps") + "</h3>";
+		res += "<h3>Name: " + request.getParameter("name") + "</h3></html>";
+		response.getWriter().print(res);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
